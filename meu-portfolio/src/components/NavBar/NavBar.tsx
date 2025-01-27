e5b28d32b3d1f3f8b8ced50d73dd55c3477a3cb2
hugogatti <hugo.gatti@hotmail.com>
2025-01-27 20:39:28 -0300

    feat: criação da NavBar

---
+++ meu-portfolio/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,23 @@
+import { AppBar, MenuItem, styled, Toolbar } from "@mui/material"
+
+const NavBar = () => {
+
+    const StyledToobar = styled(Toolbar)(()=> ({
+        display: "flex",
+        justifyContent: "space-evenly"
+    }))
+
+    return (
+      <>
+            <AppBar position="absolute">
+                <StyledToobar>
+                <MenuItem>Sobre mim</MenuItem>
+                <MenuItem>Habilidades</MenuItem>
+                <MenuItem>Projetos</MenuItem>
+                </StyledToobar>
+            </AppBar>
+      </>
+    )
+  }
+
+export default NavBar
