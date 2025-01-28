a3e01df1a10b05f64a1481e5f7121d34d8ef3ff5
hugogatti <hugo.gatti@hotmail.com>
2025-01-27 21:06:52 -0300

    feat: add onClick no StyledButton

--- meu-portfolio/src/components/StyledButton/StyledButton.tsx
+++ meu-portfolio/src/components/StyledButton/StyledButton.tsx
@@ -3,9 +3,10 @@ import { ReactNode } from "react"
 
 interface StyledButtonProps {
     children: ReactNode
+    onClick: () => void
 }
 
-const StyledButton: React.FC<StyledButtonProps> = ({children}) => {
+const StyledButton: React.FC<StyledButtonProps> = ({children, onClick}) => {
 
     //css para a imagem
     const StyledButton = styled("button")(({ theme })=> ({
@@ -26,7 +27,7 @@ const StyledButton: React.FC<StyledButtonProps> = ({children}) => {
 
     return (
       <>
-        <StyledButton>
+        <StyledButton onClick={onClick}>
             {children}
         </StyledButton>
       </>
